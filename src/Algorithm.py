178067c32f077f4c954ba660795237794e43059f
SarthakDhingra <sdhingra@ualberta.ca>
2020-07-15 01:12:06 -0600

    added files

---
+++ src/Algorithm.py
@@ -0,0 +1,10 @@
+!#/usr/bin/env python3
+
+class TwoTeams:
+
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def getBreak(self, num_teams, num_rounds, num_breaking):
+        print("got break")
+        return None
---
+++ test/AlgorithmTests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../src/")
+
+from Algorithm import TwoTeams
+
+
+
