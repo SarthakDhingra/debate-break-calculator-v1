b14bb837e0328756286a9bdc6641a2a5c77168fb
SarthakDhingra <sdhingra@ualberta.ca>
2020-07-19 23:45:53 -0600

    added template

--- src/Algorithm.py
+++ src/Algorithm.py
@@ -8,3 +8,12 @@ class TwoTeams:
     def getBreak(self, num_teams, num_rounds, num_breaking):
         print("got break")
         return None
+
+class FourTeams:
+
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def getBreak(self, num_teams, num_rounds, num_breaking):
+        print("got break")
+        return None
