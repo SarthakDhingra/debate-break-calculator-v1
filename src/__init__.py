59f047e9cf070203a1d209fb2ab6b86d749eb926
SarthakDhingra <sartdh@hotmail.com>
2020-08-31 17:33:22 -0600

    working on form

--- src/__init__.py
+++ src/__init__.py
@@ -1,32 +1,26 @@
+
+import sys
 import os
 
-from flask import Flask
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+from flask import Flask, render_template, request
+from Algorithm import Tournament
+
+app = Flask(__name__)
+app.run(debug=True)
+
+# a simple page that says hello
+@app.route('/', methods=['POST','GET'])
+def hello():
+    print("INFORMATION")
+    print(request.form)
+    teams = request.form.get("teams", None)
+    rounds = request.form.get("rounds", None)
+    breaking = request.form.get("breaking", None)
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    print("TEAMS")
+    print(teams)
+    return render_template('form.html')
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
