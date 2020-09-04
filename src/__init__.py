f6c6cefd9f37cea98118e9547eee3607889426d1
SarthakDhingra <sdhingra@ualberta.ca>
2020-09-03 18:28:35 -0600

    added env file

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,6 @@ from wtforms import IntegerField, Form, StringField
 from wtforms.validators import NumberRange, DataRequired, ValidationError
 from Algorithm import Tournament
 
-app = Flask(__name__)
 
 class Breaking(object):
     def __init__(self, message=None):
@@ -25,42 +24,47 @@ class BreakForm(Form):
     rounds = IntegerField('rounds', validators=[NumberRange(min=1,max=9, message="Rounds must be an integer between 1 and 9 inclusive")])
     breaking = StringField('breaking', validators=[Breaking(message="Number Breaking is not divisible by Style")])   
 
-@app.route('/', methods=['POST','GET'])
-def root():
+def create_app():
+    app = Flask(__name__)
 
-    teams = request.form.get("teams", None)
-    rounds = request.form.get("rounds", None)
-    breaking = request.form.get("breaking", None)
-    style = request.form.get("style", None)
+    @app.route('/', methods=['POST','GET'])
+    def root():
 
-    if style is not None:
-        style = int(style)
+        teams = request.form.get("teams", None)
+        rounds = request.form.get("rounds", None)
+        breaking = request.form.get("breaking", None)
+        style = request.form.get("style", None)
 
-    form = BreakForm(request.form)
-    form.style = style
+        if style is not None:
+            style = int(style)
 
-    if teams and rounds and breaking and style and form.validate():   
-        tournament = Tournament(style)
-        results_best, results_worst = tournament.get_Break(teams=int(teams),rounds=int(rounds),breaking=int(breaking))
-        return redirect(url_for('results',best=results_best,worst=results_worst, teams=teams, rounds=rounds, breaking=breaking))
+        form = BreakForm(request.form)
+        form.style = style
 
-    return render_template('form.html', form=form, teams=teams, rounds=rounds, breaking=breaking)
+        if teams and rounds and breaking and style and form.validate():   
+            tournament = Tournament(style)
+            results_best, results_worst = tournament.get_Break(teams=int(teams),rounds=int(rounds),breaking=int(breaking))
+            return redirect(url_for('results',best=results_best,worst=results_worst, teams=teams, rounds=rounds, breaking=breaking))
 
-@app.route('/results', methods=['GET'])
-def results():
-    results_best = request.args.get("best", None)
-    results_worst = request.args.get("worst", None)
+        return render_template('form.html', form=form, teams=teams, rounds=rounds, breaking=breaking)
 
-    if results_best is not None and results_worst is not None:
-        results_best = ast.literal_eval(results_best)
-        results_worst = ast.literal_eval(results_worst)
+    @app.route('/results', methods=['GET'])
+    def results():
+        results_best = request.args.get("best", None)
+        results_worst = request.args.get("worst", None)
+
+        if results_best is not None and results_worst is not None:
+            results_best = ast.literal_eval(results_best)
+            results_worst = ast.literal_eval(results_worst)
+        
+        teams = request.args.get("teams", None)
+        rounds = request.args.get("rounds", None)
+        breaking = request.args.get("breaking", None)
+        style = request.args.get("tournament", None)
+        
+        return render_template('form.html', results_best=results_best, results_worst=results_worst, teams=teams, rounds=rounds, breaking=breaking)
     
-    teams = request.args.get("teams", None)
-    rounds = request.args.get("rounds", None)
-    breaking = request.args.get("breaking", None)
-    style = request.args.get("tournament", None)
-    
-    return render_template('form.html', results_best=results_best, results_worst=results_worst, teams=teams, rounds=rounds, breaking=breaking)
+    return app
 
 
 if __name__ == '__main__':
