8761509d6e39f80f06e449ad11eeb1026d709f56
SarthakDhingra <sdhingra@ualberta.ca>
2020-09-03 22:12:56 -0600

    bp all good

--- src/Algorithm.py
+++ src/Algorithm.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 np.set_printoptions(linewidth=300)
+from math import floor, ceil
 
 class Tournament:
 
@@ -18,75 +19,154 @@ class Tournament:
         # make teams divisible by 4
         while(teams% self.style != 0):
             teams += 1
-        
-        tournament_best = [0 for i in range(teams)]
-        tournament_worst = [0 for i in range(teams)]
 
-        for round in range(rounds):
-            for team in range(0,teams):
-                if (team % self.style == 0):
-                    for i in range(1,self.style):
-                        tournament_best[team+i] += i
-                        tournament_worst[team + i - 1] += self.team_map[i-1]
+        # come up with better name for 2d array
+        max_points = rounds*3
 
-            tournament_best.sort()
-            tournament_worst.sort()
+
+        # each row represents a round
+        # each column represents the number of points
+        # rounds are indexed from 0 (i.e. row 0 = round 1)
+        # points start at 0
+        # each value represents the number of teams after round (row #) on the column number of points
+        tournament = [[0 for i in range(max_points+1)] for i in range(rounds)]
+
+        self.fill_data(tournament, rounds, teams)
 
         if self.verbose:
-            print(f"TOURNAMENT BEST = {tournament_best}")
-            print(f"TOURNAMENT WORST = {tournament_worst}")
+            print(np.array(tournament))
         
-        results_best = self.get_results(tournament=tournament_best,teams=teams,breaking=breaking)
-        results_worst = self.get_results(tournament=tournament_worst,teams=teams,breaking=breaking)
+        results_best, results_worst = self.get_results(tournament=tournament[-1],teams=teams,breaking=breaking)
 
         if convert_string:
-            results_best = f"All teams on {results_best.get('guranteed_break')} points will break. {results_best.get('breaking_on_speaks')} out of {results_best.get('total_on_speaks')} teams on {results_best.get('speaks_break')} points will break"
-            results_worst = f"All teams on {results_worst.get('guranteed_break')} points will break. {results_worst.get('breaking_on_speaks')} out of {results_worst.get('total_on_speaks')} teams on {results_worst.get('speaks_break')} points will break"
+            results_best = f"All teams on {results_best.get('guranteed_break')} points will break. {results_best.get('breaking_on_speaks')} out of \
+            {results_best.get('total_on_speaks')} teams on {results_best.get('speaks_break')} points will break"
+            results_worst = f"All teams on {results_worst.get('guranteed_break')} points will break. {results_worst.get('breaking_on_speaks')} out of \
+            {results_worst.get('total_on_speaks')} teams on {results_worst.get('speaks_break')} points will break"
           
+        return results_best, results_worst
+    
+    def fill_data(self, data, rounds, teams):
+
+        for i in range(4):
+            data[0][i] = teams / 4
+
+        # starting from round 2
+        for round in range(1, rounds):
+            round_max = (round+1)*3
+            for point in range(round_max+1):
+                teams = 0
+                if point == 0:
+                    teams = data[round-1][0] / 4
+                else: 
+                    for child in range(point-3,point+1):
+                        if child < 0:
+                            continue
+                        teams += data[round-1][child] / 4
+                
+                data[round][point] = teams
+
+    # add up all rounded up points for that round
+    # the highest point while it still smaller than number breaking is guranteed break
+    # the point after is the cut off
+    # the number breaking from this is (x - summation of all points until guranteed break) out of R where R can be ceil or floor of cutoff number
+    # have to explain why R can either or if its best case, why not ceil (because there are break cases )
+
+    # BEST CASE
+    # same   but with rounded dow
+
+    def get_results(self,tournament,teams,breaking):
+
+        #WANT
+        # guranteed break
+        # speaks break
+        # breaking on speaks
+        # total on speaks
+
+
+        print(f"TOURANEMNT={tournament}")
+
+        #WORST CASE
+        results_worst = {}
+
+        teams_broke = 0
+        teams_broke_prev = 0
+        point = len(tournament)-1
+
+        while teams_broke < breaking:
+            teams_broke_prev = teams_broke
+            teams_broke += ceil(tournament[point])
+            point-=1
+        
+        results_worst['speaks_break'] = point + 1
+        results_worst['guranteed_break'] = point + 2
+        results_worst['total_on_speaks'] = ceil(tournament[point+1])
+        results_worst['breaking_on_speaks'] = breaking - teams_broke_prev
+
+        #BEST CASE
+        results_best = {}
+
+        teams_broke = 0
+        teams_broke_prev = 0
+        point = len(tournament)-1
+
+        while teams_broke < breaking:
+            teams_broke_prev = teams_broke
+            teams_broke += floor(tournament[point])
+            point-=1
+        
+        results_best['speaks_break'] = point + 1
+        results_best['guranteed_break'] = point + 2
+        results_best['total_on_speaks'] = ceil(tournament[point+1])
+        results_best['breaking_on_speaks'] = breaking - teams_broke_prev
 
         return results_best, results_worst
 
-    def get_results(self,tournament,teams,breaking):
-        break_index = len(tournament) - breaking
 
-        points = []
-        results = {}
 
-        for i in range(-breaking,1):
-            if len(points) == 2:
-                break
-            if tournament[i] not in points:
-                points.append(tournament[i])
+
+
+
+        # break_index = len(tournament) - breaking
+
+        # points = []
+        # results = {}
+
+        # for i in range(-breaking,1):
+        #     if len(points) == 2:
+        #         break
+        #     if tournament[i] not in points:
+        #         points.append(tournament[i])
         
-        # can probably simplify this
-        if len(points) == 1:
-            if tournament[break_index-1] < tournament[break_index]:
-                points = [tournament[break_index], points[0]]
-            else:
-                points = [points[0],"No Guranteed Break"]
-        elif tournament[break_index-1] < tournament[break_index]:
-            points = [tournament[break_index-1], tournament[break_index]]
+        # # can probably simplify this
+        # if len(points) == 1:
+        #     if tournament[break_index-1] < tournament[break_index]:
+        #         points = [tournament[break_index], points[0]]
+        #     else:
+        #         points = [points[0],"No Guranteed Break"]
+        # elif tournament[break_index-1] < tournament[break_index]:
+        #     points = [tournament[break_index-1], tournament[break_index]]
 
-        results['speaks_break'] = points[0]
-        results['guranteed_break'] = points[1]
+        # results['speaks_break'] = points[0]
+        # results['guranteed_break'] = points[1]
 
-        # bug if there is no guranteed break point
-        num_speaks_break = 0
-        i = break_index
-        while i < len(tournament) and results['speaks_break'] == tournament[i]:
-            i += 1
-            num_speaks_break += 1
-        results['breaking_on_speaks'] = num_speaks_break
+        # # bug if there is no guranteed break point
+        # num_speaks_break = 0
+        # i = break_index
+        # while i < len(tournament) and results['speaks_break'] == tournament[i]:
+        #     i += 1
+        #     num_speaks_break += 1
+        # results['breaking_on_speaks'] = num_speaks_break
 
-        total_speaks_break = 0
-        i = break_index-1
-        while i >= 0  and tournament[i] == results['speaks_break']:
-            i += 1
-            total_speaks_break += 1
+        # total_speaks_break = 0
+        # i = break_index-1
+        # while i >= 0  and tournament[i] == results['speaks_break']:
+        #     i += 1
+        #     total_speaks_break += 1
         
-        results['total_on_speaks'] = total_speaks_break + num_speaks_break
+        # results['total_on_speaks'] = total_speaks_break + num_speaks_break
 
-        return results
+        # return results
 
 
 
--- test/AlgorithmTests.py
+++ test/AlgorithmTests.py
@@ -22,24 +22,29 @@ class AlgorithmTests(unittest.TestCase):
         else:
             results_best,results_worst = self.TwoTeams.get_Break(teams=test["teams"], breaking=test["breaking"],rounds=test["rounds"])
             
-        print("RESULTS")
+        print("RESULTS\n")
         print(f"EXPECTED = {answer}")
         print(f"RESULTS BEST = {results_best}")
-        print(f"RESULTS WORST = {results_worst}")
+        print(f"RESULTS WORST = {results_worst}\n")
 
         self.assertLessEqual(results_best["guranteed_break"], answer["guranteed_break"],f"\nFAIL RESULT BEST\n Expected: {answer}\n Result:{results_best}")
         self.assertLessEqual(results_best['speaks_break'], answer["speaks_break"],f"\nFAILRESULT BEST\n Expected: {answer}\n Result:{results_best}")
         if results_best['speaks_break'] == answer["speaks_break"]:
-            self.assertLessEqual(results_best["breaking_on_speaks"], answer["breaking_on_speaks"],f"\nFAIL RESULT BEST\n Expected: {answer}\n Result:{results_best}")
+            self.assertLessEqual(answer["breaking_on_speaks"], results_best["breaking_on_speaks"], f"\nFAIL RESULT BEST\n Expected: {answer}\n Result:{results_best}")
         
         self.assertGreaterEqual(results_worst["guranteed_break"], answer["guranteed_break"],f"\nFAIL RESULT WORST\n Expected: {answer}\n Result:{results_worst}")
         self.assertGreaterEqual(results_worst['speaks_break'], answer["speaks_break"],f"\nFAIL RESULT WORST\n Expected: {answer}\n Result:{results_worst}")
         if results_worst['speaks_break'] == answer["speaks_break"]:
-            self.assertGreaterEqual(results_best["breaking_on_speaks"], answer["breaking_on_speaks"],f"\nFAIL RESULT WORST\n Expected: {answer}\n Result:{results_worst}")
+            self.assertGreaterEqual(answer["breaking_on_speaks"], results_worst["breaking_on_speaks"], f"\nFAIL RESULT WORST\n Expected: {answer}\n Result:{results_worst}")
     
     def test_ubciv_2019(self):
         test = {"teams":40,"breaking":8,"rounds":5}
         answer = {"guranteed_break":11, "speaks_break":10, "breaking_on_speaks":2}
+        self.run_given_test(4,test,answer)
+    
+    def test_hugill_2019(self):
+        test = {"teams":32,"breaking":8,"rounds":5}
+        answer = {"guranteed_break":10, "speaks_break":9, "breaking_on_speaks":1}
         self.run_given_test(4,test,answer)
     
     def test_hhiv_2018(self):
@@ -47,33 +52,24 @@ class AlgorithmTests(unittest.TestCase):
         answer = {"guranteed_break":11, "speaks_break":10, "breaking_on_speaks":3}
         self.run_given_test(4,test,answer)
     
-    @unittest.expectedFailure
+    # @unittest.expectedFailure
     def test_wudc_2019(self):
         test = {"teams":267,"breaking":48,"rounds":9}
         answer = {"guranteed_break":18, "speaks_break":17, "breaking_on_speaks":16}
         self.run_given_test(4,test,answer)
     
-    def test_hugill_2019(self):
-        test = {"teams":32,"breaking":8,"rounds":5}
-        answer = {"guranteed_break":10, "speaks_break":9, "breaking_on_speaks":1}
-        self.run_given_test(4,test,answer)
+
+    # @unittest.expectedFailure
+    # def test_cpnats_2018(self):
+    #     test = {"teams":28,"breaking":8,"rounds":6}
+    #     answer = {"guranteed_break":5, "speaks_break":5, "breaking_on_speaks":4}
+    #     self.run_given_test(2,test,answer)
     
-    def test_hugill_2019(self):
-        test = {"teams":32,"breaking":8,"rounds":5}
-        answer = {"guranteed_break":10, "speaks_break":9, "breaking_on_speaks":1}
-        self.run_given_test(4,test,answer)
-    
-    @unittest.expectedFailure
-    def test_cpnats_2018(self):
-        test = {"teams":28,"breaking":8,"rounds":6}
-        answer = {"guranteed_break":5, "speaks_break":5, "breaking_on_speaks":4}
-        self.run_given_test(2,test,answer)
-    
-    @unittest.expectedFailure
-    def test_cpproam_2018(self):
-        test = {"teams":12,"breaking":4,"rounds":4}
-        answer = {"guranteed_break":4, "speaks_break":3, "breaking_on_speaks":4}
-        self.run_given_test(2,test,answer)
+    # @unittest.expectedFailure
+    # def test_cpproam_2018(self):
+    #     test = {"teams":12,"breaking":4,"rounds":4}
+    #     answer = {"guranteed_break":4, "speaks_break":3, "breaking_on_speaks":4}
+    #     self.run_given_test(2,test,answer)
     
 
 if __name__ == '__main__':
