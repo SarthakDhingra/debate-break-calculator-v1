7f23b9b54457cc49d2747f979484c38f44de0ddd
SarthakDhingra <sdhingra@ualberta.ca>
2020-09-03 23:19:07 -0600

    removed comments

--- src/Algorithm.py
+++ src/Algorithm.py
@@ -63,12 +63,6 @@ class Tournament:
                 
                 data[round][point] = teams
 
-    # add up all rounded up points for that round
-    # the highest point while it still smaller than number breaking is guranteed break
-    # the point after is the cut off
-    # the number breaking from this is (x - summation of all points until guranteed break) out of R where R can be ceil or floor of cutoff number
-    # have to explain why R can either or if its best case, why not ceil (because there are break cases )
-
     def get_results(self,tournament,teams,breaking):
         
         if self.verbose:
@@ -109,73 +103,6 @@ class Tournament:
         results_best['breaking_on_speaks'] = breaking - teams_broke_prev
 
         return results_best, results_worst
-
-
-
-
-
-
-        # break_index = len(tournament) - breaking
-
-        # points = []
-        # results = {}
-
-        # for i in range(-breaking,1):
-        #     if len(points) == 2:
-        #         break
-        #     if tournament[i] not in points:
-        #         points.append(tournament[i])
-        
-        # # can probably simplify this
-        # if len(points) == 1:
-        #     if tournament[break_index-1] < tournament[break_index]:
-        #         points = [tournament[break_index], points[0]]
-        #     else:
-        #         points = [points[0],"No Guranteed Break"]
-        # elif tournament[break_index-1] < tournament[break_index]:
-        #     points = [tournament[break_index-1], tournament[break_index]]
-
-        # results['speaks_break'] = points[0]
-        # results['guranteed_break'] = points[1]
-
-        # # bug if there is no guranteed break point
-        # num_speaks_break = 0
-        # i = break_index
-        # while i < len(tournament) and results['speaks_break'] == tournament[i]:
-        #     i += 1
-        #     num_speaks_break += 1
-        # results['breaking_on_speaks'] = num_speaks_break
-
-        # total_speaks_break = 0
-        # i = break_index-1
-        # while i >= 0  and tournament[i] == results['speaks_break']:
-        #     i += 1
-        #     total_speaks_break += 1
-        
-        # results['total_on_speaks'] = total_speaks_break + num_speaks_break
-
-        # return results
-
-
-
-
-    #     results = {}
-    #     i = teams-breaking
-    #     breaking_point = tournament[i]
-
-    #     # no one breaks on speaks, and guranteed_break point is last breaking team
-    #     if breaking_point > tournament[i-1]:
-    #         results['guranteed_break'] = breaking_point
-    #         results['speaks_break'] = tournament[i-1]
-    #         results['breaking_on_speaks'] = 0
-    #         results['total_on_speaks'] = self.get_count(tournament,i-1,left=True)
-    #     else:
-    #         results['speaks_break'] = tournament[i]
-    #         results['breaking_on_speaks'] = 
-    #         results['']
-
-    #     return results  
-    # def get_count(tournament,index,left=False,right=False):
 
 
         
