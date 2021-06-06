5a3d6e6ac25f625427dc535fefb20117e8f4ec82
Sarthak Dhingra <sdhingra@ualberta.ca>
2021-06-06 14:14:17 -0600

    finished form results processing

--- src/components/BreakForm.js
+++ src/components/BreakForm.js
@@ -4,25 +4,40 @@ app.component('break-form', {
     template:
     /*html*/
     `
-    <form class='break-form' @submit.prevent="onSubmit">
+    <form class='break-form' @submit.prevent="onSubmit($event)">
         <h3> This is the Form</h3>
         <label for="teams">Number of TeamsS</label>
-        <input id="teams">
+        <input id="teams" v-model.number="teams" >
         <label for="rounds">Number of Preliminary Rounds </label>
-        <input id="rounds">
+        <input id="rounds" v-model.number="rounds">
         <label for="breaking">Number Advancing to Outrounds</label> 
-        <input id="breaking">
+        <input id="breaking" v-model.number="breaking">
 
-        <input class="button" type="submit" value="Submit">  
-        <input class="button" type="submit" value="Submit">  
+        <button name="style" value=2 type="submit" v-bind="style">Two Teams</button>
+        <button name="style" value=4 type="submit" v-bind="style">Four Teams</button> 
     </form>
 
 
     `,
-
+    data() {
+        return {
+            teams: 40,
+            rounds: 5,
+            breaking: 8,
+        }
+    },
     methods: {
-        onSubmit() {
+        onSubmit(event) {
+
             console.log("TESTING")
+            let style = event.submitter.attributes.value.value
+            console.log(style) // 4
+            console.log(this.teams) // 40
+            console.log(this.rounds) // 5
+            console.log(this.breaking) // 8
+            let results = this.getResults(style, this.teams, this.breaking, this.rounds)
+            console.log("RESULTS")
+            console.log(results)
         }
     }
 })
